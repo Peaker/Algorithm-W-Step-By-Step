ade9124ea93b2262bf50487b8d7e7b66c6ebb76a
Eyal Lotem <eyal.lotem@gmail.com>
2016-03-29 14:02:02 +0300

    Data.ByteString.Hex: Add parse exports

--- Data/ByteString/Hex.hs
+++ Data/ByteString/Hex.hs
@@ -1,9 +1,14 @@
 module Data.ByteString.Hex
     ( showHexByte
     , showHexBytes
+    , parseHexDigit
+    , parseHexByte
+    , parseHexDigits
     ) where
 
+import           Control.Lens.Operators
 import qualified Data.ByteString as SBS
+import qualified Data.Char as Char
 import           Data.Word (Word8)
 import           Text.Printf (printf)
 
@@ -12,3 +17,28 @@ showHexByte = printf "%02X"
 
 showHexBytes :: SBS.ByteString -> String
 showHexBytes = concatMap showHexByte . SBS.unpack
+
+chunks :: Int -> [a] -> Either String [[a]]
+chunks _ [] = Right []
+chunks n xs
+    | length firstChunk == n = chunks n rest <&> (firstChunk :)
+    | otherwise = Left $ unwords ["Expecting chunks of size", show n, "found", show (length firstChunk)]
+    where
+        (firstChunk, rest) = splitAt n xs
+
+parseHexDigit :: Char -> Either String Word8
+parseHexDigit x
+    | Char.isHexDigit x = Right (fromIntegral (Char.digitToInt x))
+    | otherwise = Left $ "Expecting digit, found " ++ show x
+
+parseHexByte :: String -> Either String Word8
+parseHexByte [x,y] =
+    (+)
+    <$> (parseHexDigit x <&> (16 *))
+    <*> parseHexDigit y
+parseHexByte other =
+    Left $ unwords ["Expecting two hex digits, found", show (length other), "characters"]
+
+parseHexDigits :: String -> Either String SBS.ByteString
+parseHexDigits str =
+    chunks 2 str >>= mapM parseHexByte <&> SBS.pack
